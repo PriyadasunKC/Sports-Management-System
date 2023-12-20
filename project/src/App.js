02f1a7c648080dd07dcae9b576cd57d1c031efc2
Chathura <cpriydasun@gmail.com>
2023-12-20 20:22:09 +0530

    2nd commit

---
+++ project/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import UserApplication from "./Components/UserApplication/UserApplication.jsx";
+import UserApplicationTable from "./Components/UserApplicationTable/UserApplicationTable.jsx";
+import StatCards from "./Components/StatCard/StatCards.jsx";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/AdminDashboard" element={<StatCards />} />
+        <Route
+          path="/AdminDashboard/UserValidation"
+          element={<UserApplicationTable />}
+        />
+        <Route
+          // Need to change after add table of application
+          path="/AdminDashboard/UserValidation/view"
+          element={<UserApplication/>}
+        />
+      </Routes>
+    </BrowserRouter>
+
+    // <div className="App">
+
+    //   <header className="App-header">
+    //     <Navbar/>
+
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,65 +1,23 @@
-const  express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-// import the user model
-const UserApplicationModel = require('./models/userApplication');
-const UserApplicationTableModel = require('./models/userApplicationTable');
+require("dotenv").config(); // Load environment variables
 
+const express = require("express");
+const cors = require("cors");
 
+const authRouter = require("./router/userRouter");
+const adminRouter = require("./router/adminRouter");
+
+require("./db/mongoDb"); 
 
 const app = express();
+
+app.use(express.json()); // middlewares
 app.use(cors());
-app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://adminsms:admin123@cluster0.3yvr8xj.mongodb.net/userApplicationModel"
-);
+app.use("/api/v1/user/", authRouter);
+app.use("/api/v1/admin", adminRouter);
 
-const connection = mongoose.connection;
-connection.once("open", function () {
-  console.log("MongoDB database connected");
+const PORT = process.env.PORT || 3000; // Define a default port if PORT is not set in .env
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 });
-
-// API to access users
-// req is the data we passing to the server
-// res is the response we get from the server
-// Using created model to access the database
-app.get("/getUserApplicationData", async (req, res) => {
-  UserApplicationModel.find({})
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-// API to add data to the database
-app.post("/addUserApplicationData", async (req, res) => {
-  UserApplicationModel.create(req.body)
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error("Error:", err);
-      res.status(400).json("Error: " + err);
-    });
-});
-
-// API to access the userApplication Table Data
-app.get("/getUserApplicationTableData", async (req, res) => {
-  UserApplicationTableModel.find({})
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-// Create the server
-app.listen(5000, () => {
-    console.log('Server has started on port 5000');
-});
-
